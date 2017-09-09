a16c178f542291b372faa99801c87f1a9a7dd2e3
Jacob Allen <jedijake@me.com>
2017-09-09 14:47:14 -0700

    generate navbar controller, declair navbar sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdCardModule, MdButtonModule, MdButtonToggleModule } from '@angular/material';
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
