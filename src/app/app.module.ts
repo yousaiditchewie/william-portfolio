36c93a80b267448f22ba982b1d8ea58f6a9fe246
Jacob Allen <jedijake@me.com>
2017-09-09 11:31:20 -0700

    initialize repo, install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCardModule, MdButtonModule, MdButtonToggleModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdCardModule,
+    MdButtonModule,
+    MdButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
